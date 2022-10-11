87e4929696af7f5ba5111cbb187986edd0aa86f6
Dennis Yilmaz <dy.0x8@outlook.com>
2022-10-11 00:54:17 -0400

    Add Output for Disassembler

--- disassembler/main.cpp
+++ disassembler/main.cpp
@@ -13,13 +13,13 @@ void print_header() {
     printf("╰╯╱╱╱╱╰━━╯\n");
 }
 void print_options() {
-    printf("1. Extract IHDR\n");
-    printf("2. Extract IDAT\n");
+    printf("1. Extract Chunk\n");
+    printf("2. Extract Disassemble\n");
     printf("3. Extract Chunks to File\n");
     printf("4. Exit\n");
 }
 
-void handleFileExtration() {
+void handleBaseDisassemble() {
     printf("\x1b[23H\x1b[2J");
     printf("please enter the file name: \n");
     printf("(make sure it is in the same folder)\x1b[A\x1b[23;29H");
@@ -31,6 +31,20 @@ void handleFileExtration() {
         ichars[i] = input[i];
     }
     Disassembler::disassemble(ichars);
+}
+
+void handleExtractDisassemble() {
+    printf("\x1b[23H\x1b[2J");
+    printf("please enter the file name: \n");
+    printf("(make sure it is in the same folder)\x1b[A\x1b[23;29H");
+    std::string input;
+    std::cin >> input;
+    printf("\n");
+    char ichars[sizeof(input)];
+    for(int i = 0; i < sizeof(input); i++) {
+        ichars[i] = input[i];
+    }
+    Disassembler::disassemble_output(ichars);
 }
 
 int main() {
@@ -46,9 +60,10 @@ int main() {
         if(strcmp(input, "1") == 0) {
         }
         if(strcmp(input, "2") == 0) {
+            handleBaseDisassemble();
         }
         if(strcmp(input, "3") == 0) {
-            handleFileExtration();
+            handleExtractDisassemble();
         }
         if(strcmp(input, "4") == 0) {
             exit(1);
--- disassembler/source/disassembler.cpp
+++ disassembler/source/disassembler.cpp
@@ -110,4 +110,98 @@ namespace Disassembler {
         std::cin >> x;
         return 1;
     }
+    
+    int disassemble_output(const char file[]) {
+        // input file
+        std::ifstream f(file, std::ios::out | std::ios::binary);
+        
+        // output file 
+        std::ofstream o("disassembled.txt", std::ios::out | std::ios::binary);
+        if(!f.is_open() || !f) {
+            printf("could not find file..\n");
+            return 0;
+        } else {
+            printf("found file.. %s\n", file);
+        }
+
+        printf("current pos: %d\n", (int)f.tellg());
+        // Determine file size
+        f.seekg(0, f.end);
+        int fileSize = f.tellg();
+        f.seekg(0, f.beg);
+
+        // Read file signature
+        _FileSig fileSignature;
+        f.read((char *)&fileSignature, sizeof(_FileSig));
+        printf("read sig: ");
+        for(int i = 0; i < sizeof(fileSignature.signature); i++) {
+            printf("%d ", fileSignature.signature[i]);
+        }
+        printf("\ntotal Size: %d\n", fileSize);
+        
+        int z = 0;
+        printf("------------------------------\n");
+        // Read the file until we reach the end
+        while (!(f.tellg() == fileSize))
+        {   
+            // The data chunk (found in headers/structs.h)
+            _Chunk chunk;
+
+            printf("chunk read position: %d\n", (int)f.tellg());
+            
+            // Read the data chunk
+            f.read((char *)&chunk, sizeof(chunk));
+            // Go from reverse byte order to host byte order (png files have the bytes written in reverse)
+            int chunkSize = ntohl(chunk.length);
+
+            // Print outputs
+            printf("chunk size: %d\n", chunkSize);
+            printf("chunk type: %s\n", chunk.type);
+        
+            if(chunkSize != 0) {
+                // Create a new byte buffer with the chunkSize + 4 bytes (the size of the crc)
+                // char * buffer[chunkSize + 4];
+                char * heap_ptr = nullptr;
+                if(chunkSize + 4 != 4) {
+                    printf("allocating memory page..\n");
+                    heap_ptr = (char *)malloc(chunkSize); 
+                    printf("done.. \n");
+                }
+
+                // Read the bytes to the buffer, then append them to the data vector
+                printf("projected read position: %d\n", (int)f.tellg()+ chunkSize + 4);
+                printf("reading chunk...\n");
+                f.read((char *)heap_ptr, chunkSize);
+                o.write(heap_ptr, chunkSize);
+                printf("done!\n");
+                if(chunkSize < 100) {
+                    printf("raw hex bytes: ");
+                    int x;
+                    std::cin >> x;
+                    if(chunkSize != 0) {
+                        for(int i = 0; i < chunkSize; i++) {
+                            printf("%x ", heap_ptr[i]);
+                        }
+                    }           
+                }
+                printf("\n");
+                printf("heap address: %x\n", &heap_ptr);
+            }
+            char crc[4];
+            printf("crc: ");
+            for(int i = 0; i < sizeof(crc); i++) {
+                printf("%x ", crc[i]);
+            }
+            printf("\n");
+            f.read((char *)&crc, sizeof(crc));
+            printf("chunk read position: %d\n", (int)f.tellg());
+            printf("------------------------------\n");
+            o.write("\n------------------------------\n", 32);
+            z++;
+        }
+        printf("total chunks read: %d\n", z);
+        int x;
+        std::cin >> x;
+        return 1;
+    }
 }
